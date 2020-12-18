d321020a2260f608213fe75586a397930e258d87
Toan Nguyen <tnguyen@upbooo.com>
2020-12-18 23:26:02 +0700

    add: unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import GeoLocation from './GeoLocation';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+	shallow(<App />);
+});
+
+it('renders GeoLocation without crashing', () => {
+	shallow(<GeoLocation />);
 });
